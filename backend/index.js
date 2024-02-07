8c50913958ac57298cf56dac175ae6f8526da3f5
saurabh kumar <saurabhkumar9493@gmail.com>
2024-02-07 11:02:15 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,27 @@ app.post('/createUser',(req,res)=>{
 app.delete('/deleteUser/:kuchbhi',(req,res)=>{
             const id  = req.params.kuchbhi;
             UserModel.findByIdAndDelete({_id:id})
-            .then(res=>res.json(res))
+            .then(users=>res.json(users))
             .catch(err=>res.json(err))
+})
+
+app.put('/updateUser/:id',(req,res)=>{
+        const id = req.params.id
+         UserModel.findByIdAndUpdate({_id:id},{
+          name: req.body.name,
+          email: req.body.email,
+          age: req.body.age,
+         })
+        .then(users=>res.json(users))
+        .catch(err=>res.json(err))
+
+})
+
+app.get('/getUser/:id',(req,res)=>{
+  const id = req.params.id;
+  UserModel.findById({_id:id})
+  .then(users => res.json(users))
+  .catch(err=>res.json(err))
 })
 
 app.listen(8000,()=>{
